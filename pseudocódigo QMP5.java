3bcb8088feb3ef81f01a914bfc55e023bcbbe870
Ailén González Pérez <ailengonzalezperez2@gmail.com>
2021-05-24 16:55:49 -0300

    pseudocódigo diagrama clases v2

---
+++ pseudocódigo QMP5.java
@@ -0,0 +1,97 @@
+
+public class Prenda{
+	//funcionalidad de iteraciones anteriores
+}
+
+public class Guardarropas{
+	private String nombre;
+	public List<Prenda> prendas;
+  
+	public Guardarropas(String nombre){
+		this.nombre = nombre;
+		this.prendas = new ArrayList<Prenda>;
+	}
+  
+	public void agregarPrenda(Prenda prenda){
+		this.prendas.add(prenda);
+	}  
+  
+	public void eliminarPrenda(Prenda prenda){
+		this.prendas.remove(prenda);
+	} 
+}
+
+public class Usuario{
+	public List<Guardarropas> guardarropas;
+	public List<Sugerencias> sugerenciasPendientes;
+	public List<Sugerencias> sugerenciasAceptadas;
+	
+	public void agregarPrenda(Prenda prenda, Guardarropas guardarropas){
+		guardarropas.agregarPrenda(prenda);
+	}
+	
+	public void eliminarPrenda(Prenda prenda, Guardarropas guardarropas){
+		guardarropas.eliminarPrenda(prenda);
+	}
+	
+	public void revisarSugerenciasDeModificacion(){
+		aquí para cada sugerencia pendiente decido
+		si acepta
+			sugerenciasAceptadas.add(sugerenciaActual);
+			sugerenciasPendientes.remove(sugerenciaActual);
+			sugerenciaActual.tipoSugerencia.concretarSugerencia(prenda,guardarropa); //la persona elige el guardarropas de esa prenda
+		si no acepta
+			sugerenciaPendientes.remove(sugerenciaActual)
+	}
+	
+	public void deshacerSugerenciaAceptada(Sugerencia sugerencia){
+		sugerencia.tipoSugerencia.deshacerSugerencia(prenda);
+		sugerenciasPendientes.add(sugerencia);
+		sugerenciasAceptadas.remove(sugerencia);
+	}
+	
+	public void compartirGuardarropasCon(Usuario usuario, Guardarropas guardarropas){
+		usuario.agregarGuardarropas(guardarropas);
+	}
+	
+	public void agregarUnGuardarropas(Guardarropas guardarropas){
+		this.guardarropas.add(guardarropas)
+	}
+	
+	public void sugerirPrenda(Prenda prenda, Usuario usuario, TipoSugerencia tipo){
+		usuario.recibirSugerencia(new Sugerencia(prenda, tipo, this);
+	}
+	
+	public void recibirSugerencia(Sugerencia sugerencia){
+		this.sugerenciasPendientes.add(sugerencia);
+	}
+} 
+
+public class Sugerencia{ //Data class que me permite ganar abstracción
+	Usuario usuario sugeridor;
+	TipoSugerencia tipo;
+	Prenda prenda;
+	
+	public Sugerencia(Prenda prenda, TipoSugerencia tipo, Usuario usuarioSugeridor){
+		this.prenda = prenda;
+		this.tipo = tipo;
+		this.usuarioSugeridor = usuarioSugeridor;
+	}
+}
+
+public enum TipoSugerencia{
+	AGREGAR, ELIMINAR
+	
+	AGREGAR.concretarSugerencia(Prenda prenda, Guardarropas guardarropa){
+		guardarropas.agregarPrenda(prenda);
+	}
+	AGREGAR.deshacerSugerencia(Prenda prenda, Guardarropas guardarropa){
+		guardarropas.eliminarPrenda(prenda);
+	}
+	
+	ELIMINAR.concretarSugerencia(Prenda prenda, Guardarropas guardarropa){
+		guardarropas.eliminarPrenda(prenda);
+	}
+	ELIMINAR.deshacerSugerencia(Prenda prenda, Guardarropas guardarropa){
+		guardarropas.agregarPrenda(prenda);
+	}
